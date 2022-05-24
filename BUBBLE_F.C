c27b4cbd5b46c161e85fcf39d58892823209c262
koushikMahato <98101659+koushikMahato@users.noreply.github.com>
2022-05-24 23:28:07 +0530

    Add files via upload

---
+++ BUBBLE_F.C
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>
+
+void Bubblesort(int arr[],int size)
+{
+	int i,j,temp;
+	for(i=0;i<(size-1);i++)
+	{
+		for(j = 0;j<(size-1-i);j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	for(i=0;i<size;i++)
+	printf("%d\t",arr[i]);
+}
+
+int main()
+{
+	int arr[100],size,i;
+	clrscr();
+	printf("Enter array size: ");
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		printf("Enter %dth number element: ",i);
+		scanf("%d",&arr[i]);
+	}
+	Bubblesort(arr,size);
+	getch();
+	return 0;
+}
