9f5f1da71e4c0a3cff8cb5666ff1274f20a93215
koushikMahato <98101659+koushikMahato@users.noreply.github.com>
2022-05-24 23:29:45 +0530

    Add files via upload

---
+++ INSER.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void insertionsort(int arr[],int size)
+{
+	int i,j,k,temp;
+	for(i=1;i<size;i++)
+	{
+		temp=arr[i];
+		for(j=i-1;j>=0 && arr[j]>temp;j--)
+		{
+			arr[j+1]=arr[j];
+		}
+		arr[j+1]=temp;
+	}
+
+}
+int main()
+{
+	int arr[100],size,i;
+	clrscr();
+	printf("Enter array size: ");
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		printf("Enter %dth number element: ",i);
+		scanf("%d",&arr[i]);
+	}
+	insertionsort(arr,size);
+	for(i=0;i<size;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	getch();
+	return 0;
+}
