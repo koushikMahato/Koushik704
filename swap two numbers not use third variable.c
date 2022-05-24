2bc94e341cb36f52e71dcdc6f87226457cf0a62f
koushikMahato <98101659+koushikMahato@users.noreply.github.com>
2022-05-24 16:15:30 +0530

    Add files via upload

---
+++ swap two numbers not use third variable.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a=10,b=20;
+{
+	 a=a+b;
+	 b=a-b;
+	 a=a-b;
+	 printf("swap two numbers not use third variabl=%d\t%d\n\n\n",a,b);
+	
+}
+
+{
+	a=a*b;
+	b=a/b;
+	a=a/b;
+	printf("swap two numbers not use third variabl=%d\t%d",a,b);
+	
+}
+}
+
